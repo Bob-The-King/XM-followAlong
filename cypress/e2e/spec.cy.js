94cb9cc96e2f32566864ca07cb7fedac63bf3853
Bob-The-King <C2022Hill@gmail.com>
2024-09-17 20:41:04 -0600

    create testimonials

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,9 +1,41 @@
+import generateTestimonial from "../../util/generate-testimonial"
+
 describe('testimonials', () => {
-  it('passes', () => {
+  it('lists testimonials', () => {
     cy.visit('localhost/XM-followAlong')
 
     cy.get("h1").should("have.text", "Testimonials")
 
     cy.get("ul[name=testimonials-list]").should("be.visible")
   })
+
+  it("create a testimonial", () => {
+    cy.visit('localhost/XM-followAlong')
+    
+    cy.get("form").should("be.visible");
+
+    const fakeTestimonial = generateTestimonial();
+    const {feedback: testFeedback, rating: testRating} = fakeTestimonial;
+    // const testFeedback = "Example Feedback";
+    // const testRating = 5;
+
+    cy.get("form input[name=feedback]").type(testFeedback);
+    cy.get("form input[name=rating]").type(testRating);
+
+    cy.get("form input[type=submit]").click();
+
+    cy.url().should("eq", "http://localhost/XM-followAlong/create.php") ; 
+
+    cy.contains("Testimonial Created!");
+
+    cy.get("a").should("have.text", "Return to Testimonials").click();
+
+    cy.url().should("eq", "http://localhost/XM-followAlong/index.php");
+    
+    cy.get("ul[name=testimonials-list] li")
+    .last()
+    .should("have.text", `${testFeedback} ${testRating}/5`);
+    })
+  
 })
+
--- index.php
+++ index.php
@@ -19,4 +19,16 @@
             
         ?>
     </ul>
+
+    <form action="create.php" method="POST">
+        <label for="feedback">Feedback:</label>    
+        <input name="feedback" type="text">
+
+        <label for="rating">Rating:</label>
+        <input name="rating" type="number" min="0" max="5">
+
+        <input type="submit">
+
+    </form>
+
 </html>
